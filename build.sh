e04bf4ccfef3d0c79feaad1d7ac41d12fb61cd92
MattBrumpton <matt@mb-comp.co.uk>
2023-09-12 08:30:40 +0100

    Initial Changes

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e -o pipefail
 
 # jdk-8
 
-docker build --pull -t maven-chrome:jdk-8 jdk-8
-TAG=jdk-8 bats test
+# docker build --pull -t maven-chrome:jdk-8 jdk-8
+# TAG=jdk-8 bats test
 
 # jdk-11
 
@@ -14,10 +14,10 @@ TAG=jdk-11 bats test
 
 # jdk-17
 
-docker build --pull -t maven-chrome:jdk-17 jdk-17
-TAG=jdk-17 bats test
+# docker build --pull -t maven-chrome:jdk-17 jdk-17
+# TAG=jdk-17 bats test
 
 # jdk-20
 
-docker build --pull -t maven-chrome:jdk-20 -t maven-chrome jdk-20
-TAG=jdk-20 bats test
+# docker build --pull -t maven-chrome:jdk-20 -t maven-chrome jdk-20
+# TAG=jdk-20 bats test
